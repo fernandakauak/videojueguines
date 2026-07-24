4708a7db033e68ee56318969abbdac2d1700641b
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-07-24 12:22:08 -0400

    Añadido nuevo juego para vista de listado

--- src/juegos.js
+++ src/juegos.js
@@ -1,3 +1,4 @@
 export const juegos = [
   { id: 0, nombre: 'Pokémon Crystal', genero:['RPG'], anio: '2000', plataforma: ['Game Boy Color'], clasificacion: 'E', opinion: 'Muy buen juego', img: '/src/assets/imgs/juegos/pkmncrystal.jpg'},
+  { id: 1, nombre: 'Needy Girl Overdose', genero:['Novela Visual'], anio: '2012', plataforma: ['PC'], clasificacion: 'M', opinion: 'Me gusta que se vea bonito y tenga un trasfondo complejo', img: '/src/assets/imgs/juegos/needygirlov.jpg'},
 ]
