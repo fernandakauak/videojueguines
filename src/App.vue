850aac22cf66d6b76b5d8f8cee31b267186956d7
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-07-23 22:02:30 -0400

    Inicial

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './views/HelloWorld.vue'
+</script>
+
+<template>
+ <div class="demo">
+ 
+  <router-view/>
+ </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import articulosVG from '../views/articulosVG.vue';
+import VistaDataJuego from '../views/VistaDataJuego.vue';
+import VistaDataAutor from '../views/VistaDataAutor.vue';
+import SobreNosotros from '../views/SobreNosotros.vue';
+import Sitio404VG from '../views/Sitio404VG.vue';
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+
+    routes:[
+        {
+            path:'/', redirect:'/videojueguines'
+        },
+        {
+            path:'/juegos',
+            alias:['/games','/jueguines'],
+            component: articulosVG
+
+        },  
+        {
+            path:'/nosotros',
+            component: SobreNosotros
+
+        },       
+        {
+            path:'/juegos/:id',
+            props:true,
+            children:[
+                
+                {
+                    path:'data',
+                    component: VistaDataJuego,
+                    props:true
+                },
+                {
+                    path:'autor',
+                    component: VistaDataAutor,
+                    props:true
+                }
+            ] 
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            component: Vista404
+        }
+        
+    ]
+});
+
+
+export default router;
