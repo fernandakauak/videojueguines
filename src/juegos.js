d4643aa8db90810c232d74197b5a8cc9fbe095b8
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-07-23 22:22:28 -0400

    Estructura JS

---
+++ src/juegos.js
@@ -0,0 +1,3 @@
+export const juegos = [
+  { id: 0, nombre: 'Pokémon Crystal', genero:['RPG'], anio: '2000', plataforma: ['Game Boy Color'], clasificacion: 'E', opinion: 'Muy buen juego', img: 'assets/imgs/juegos/pkmncrystal.jpg'},
+]
