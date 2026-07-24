ee7e5deabf29f28e7f90c067f906829955390eaf
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-07-23 22:47:51 -0400

    CSS y empieza el listado de artículos

--- src/views/ArticulosVG.vue
+++ src/views/ArticulosVG.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<script setup>
+    import { juegos } from '../juegos.js'
+</script>
 
-<template></template>
+<template>
+    <div>
+        <h1>Lista de Artículos</h1>
 
-<style></style>
+        <div v-for="vg in juegos" :key="vg.id">
+            <h5>{{ vg.nombre }}</h5>
+            <ul>
+                <li><router-link :to="`juegos/${vg.id}/data`">Ver Data</router-link></li>
+                <li><router-link :to="`juegos/${vg.id}/autor`">Ver Autor</router-link></li>
+            </ul>
+
+        </div>
+    </div>
+</template>
