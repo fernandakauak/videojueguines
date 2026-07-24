ba91f367b8e1e2105227a1fe23ee5b7665cd1548
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-07-24 12:03:35 -0400

    Sección ArticulosVG y cambio en link de img (juegos.js)

--- src/views/ArticulosVG.vue
+++ src/views/ArticulosVG.vue
@@ -3,16 +3,61 @@
 </script>
 
 <template>
-    <div>
-        <h1>Lista de Artículos</h1>
-
-        <div v-for="vg in juegos" :key="vg.id">
-            <h5>{{ vg.nombre }}</h5>
-            <ul>
-                <li><router-link :to="`juegos/${vg.id}/data`">Ver Data</router-link></li>
-                <li><router-link :to="`juegos/${vg.id}/autor`">Ver Autor</router-link></li>
-            </ul>
-
-        </div>
-    </div>
+    <h1>Lista de Artículos</h1>
+    <section>
+        <article class="game-preview" v-for="vg in juegos" :key="vg.id">
+            <img v-bind:src="vg.img" alt="{{ vg.nombre }}">
+            <div class="data">
+                <h3>{{ vg.nombre }}</h3>
+                <button><router-link :to="`juegos/${vg.id}/data`">Ver Data</router-link></button>
+                <button><router-link :to="`juegos/${vg.id}/autor`">Ver Autor</router-link></button>
+            </div>
+        </article>
+    </section>
 </template>
+
+<style>
+    section {
+        width: 100%;
+        display: flex;
+    }
+
+    article.game-preview {
+        width: 46%;
+        padding: 2%;
+        margin: 2%;
+        display: flex;
+        background-color: indigo;
+        color: lightpink;
+    }
+
+    article.game-preview img {
+        width: 150px;
+        height: auto;
+        margin: 2%;
+    }
+
+    article.game-preview .data {
+        width: 100%;
+        text-align: center;
+        background-color: black;
+    }
+
+    article.game-preview .data button {
+        width: 100%;
+        background-color: lightpink;
+        padding: 2% 4%;
+        cursor: pointer;
+        border-radius: 10px;
+        border: none;
+        margin: 2% 0;
+        font-weight: bold;
+        clear: both;
+    }
+
+    article.game-preview .data button a {
+        text-decoration: none;
+        color: indigo;
+    }
+
+</style>
