d424c0cb3c4370a10c19ee9fcc5316996397bfe3
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-07-24 12:46:07 -0400

    sobre nosotros, estilos para enlaces del nav (activos e inactivos)

--- src/views/SobreNosotros.vue
+++ src/views/SobreNosotros.vue
@@ -1,5 +1,35 @@
-<script setup></script>
+<script setup>
+    import { useRoute, useRouter } from 'vue-router';
 
-<template></template>
+    const route= useRoute();
+    const router = useRouter();
+</script>
 
-<style></style>
+<template>
+    <section>
+        <article class="about">
+            <h3>PokeMetálicos asociados <span>presenta...</span></h3>
+            <h1>Videojueguines</h1>
+            
+            <p>Esperamos que disfrutes este sitio donde hablamos de los juegos que nos gustan, ya sean actuales o del año de la cocoa. Tenemos muchas recomendaciones para que puedas divertirte, conocer historias y, quién sabe, encuentres tu nuevo videojuego favorito.</p>
+
+            <h2>Let's play some games!</h2>
+        </article>
+    </section>
+
+</template>
+
+<style>
+    section {
+        width: 100%;
+    }
+
+    .about {
+        width: 80%;
+        margin: 2% auto;
+        background-color: indigo;
+        border-radius: 10px;
+        padding: 2%;
+        color: lightpink;
+    }
+</style>
